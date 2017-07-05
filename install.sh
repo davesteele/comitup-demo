a481f5c8a9ebe9f06dedfa089064f6d25bcbbae8
David Steele <steele@debian.org>
2017-07-05 14:28:57 -0500

    install.sh: Add motd

--- install.sh
+++ install.sh
@@ -6,5 +6,8 @@ cp demoweb /usr/local/bin/
 cp comitup-demo.service /etc/systemd/system/
 cp demoweb.service /etc/systemd/system/
 
+echo "echo Configured using Comitup" > /etc/update-motd.d/99-xfooter
+chmod 755 /etc/update-motd.d/99-xfooter
+
 systemctl enable comitup-demo.service
 systemctl start comitup-demo.service
