5d6c6c4040b48fbc816802a00e75155d1792f8e3
David Steele <dsteele@gmail.com>
2017-07-11 02:42:53 +0000

    Rename demoweb exe to demoweb.py

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 cp comitup-demo /usr/local/bin/
-cp demoweb /usr/local/bin/
+cp demoweb.py /usr/local/bin/
 
 cp comitup-demo.service /etc/systemd/system/
 cp demoweb.service /etc/systemd/system/
 
-echo "echo Configured using Comitup" > /etc/update-motd.d/99-xfooter
-chmod 755 /etc/update-motd.d/99-xfooter
+#echo "echo Configured using Comitup" > /etc/update-motd.d/99-xfooter
+#chmod 755 /etc/update-motd.d/99-xfooter
 
 systemctl enable comitup-demo.service
 systemctl start comitup-demo.service
