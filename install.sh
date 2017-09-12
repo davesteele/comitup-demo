4010d1342bd8c3dd900f6fc6b4464786944ffb1f
David Steele <davesteele@users.noreply.github.com>
2017-09-12 13:07:13 -0400

    install.sh: Edit comitup.conf for demoweb.service

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ cp demoweb.py /usr/local/bin/
 cp comitup-demo.service /etc/systemd/system/
 cp demoweb.service /etc/systemd/system/
 
+sed -i 's/\(^.\*web_service:.\+$\)/web_service: demoweb.service/' /etc/comitup.conf
+
 #echo "echo Configured using Comitup" > /etc/update-motd.d/99-xfooter
 #chmod 755 /etc/update-motd.d/99-xfooter
 
